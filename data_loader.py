3296ee031f158d58392d9b7b62727c9f5ce36030
Raghuvansh Raj <raghuvansh1997@gmail.com>
2018-08-28 21:51:12 +0530

    added all relevant files

---
+++ data_loader.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 27 22:52:00 2018
+
+@author: raghuvansh
+"""
+
+import csv
+
+def read_data():
+
+    with open('playtennis.data', 'r') as csvfile:
+        datareader = csv.reader(csvfile, delimiter=',')
+        headers = next(datareader)
+        metadata = []
+        traindata = []
+        for name in headers:
+            metadata.append(name)
+            
+        for row in datareader:
+            traindata.append(row)
+
+    return (metadata, traindata)
